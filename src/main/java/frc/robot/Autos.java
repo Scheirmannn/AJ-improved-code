1254c79c34e95d18716edfa86b03aa87118bd66f
Lucas scheirmann <lodingcid@gmail.com>
2026-04-14 09:45:20 -0400

    AUGIYDGOUDGOUGD

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -1,13 +1,41 @@
 package frc.robot;
 
+import choreo.Choreo;
+import choreo.trajectory.SwerveSample;
+import choreo.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
+import java.util.List;
+import java.util.Optional;
+
 public final class Autos {
 
     private Autos() {
+    }
+
+    private static Command buildChoreoAuto(DriveSubsystem drive, String trajectoryName) {
+        Optional<Trajectory<SwerveSample>> trajectory = Choreo.loadTrajectory(trajectoryName);
+
+        if (trajectory.isEmpty()) {
+            return Commands.print("[Autos] WARNING: Trajectory '" + trajectoryName + "' not found!");
+        }
+
+        List<SwerveSample> samples = trajectory.get().samples();
+
+        return Commands.sequence(
+                Commands.runOnce(() -> drive.resetOdometry(samples.get(0).getPose())),
+                Commands.sequence(samples.stream()
+                        .map(sample -> Commands.runOnce(() -> drive.followTrajectory(sample), drive)
+                                .andThen(Commands.waitSeconds(0.02)))
+                        .toArray(Command[]::new)),
+                Commands.runOnce(() -> drive.stopModules(), drive));
+    }
+
+    public static Command followChoreoTrajectory(DriveSubsystem drive, String trajectoryName) {
+        return buildChoreoAuto(drive, trajectoryName);
     }
 
     public static Command goBackTime(DriveSubsystem drive, ShooterSubsystem shooter) {
